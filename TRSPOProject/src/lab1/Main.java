3e6297705d54b031f7ea2e415cace72b1d2dc416
korzhanvlada <32550342+korzhanvlada@users.noreply.github.com>
2017-10-05 20:22:56 +0300

    create

---
+++ TRSPOProject/src/lab1/Main.java
@@ -0,0 +1,12 @@
+package lab1;
+
+import java.util.Arrays;
+
+/**
+ * Created by Vlada PC on 05.10.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("args = " + Arrays.toString(args));
+    }
+}
